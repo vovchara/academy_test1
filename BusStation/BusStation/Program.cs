883a00bfb6a30a603ecf39a3291c6ed81814cc2e
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2021-10-18 19:04:06 +0300

    second commit

--- BusStation/BusStation/Program.cs
+++ BusStation/BusStation/Program.cs
@@ -7,6 +7,7 @@ namespace BusStation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Bus Station!");
+            Console.WriteLine("Bye Bus Station!");
         }
     }
 }
