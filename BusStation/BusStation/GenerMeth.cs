a5cf6edcbe68f9ca59198804ac7f940980eca62c
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2021-10-19 10:07:37 +0300

    lesson 18 10 code

---
+++ BusStation/BusStation/GenerMeth.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation
+{
+    class GenerMeth
+    {
+        public void Main()
+        {
+            int x = 7;
+            int y = 25;
+            Swap(ref x, ref y); // или так Swap(ref x, ref y);
+            Console.WriteLine($"x={x}    y={y}");   // x=25   y=7
+
+            string s1 = "hello";
+            string s2 = "bye";
+            Swap(ref s1, ref s2); // или так Swap(ref s1, ref s2);
+            Console.WriteLine($"s1={s1}    s2={s2}"); // s1=bye   s2=hello
+
+            var www = Foo<string>();
+            Console.WriteLine(www);
+
+            var qqq = Foo<int>();
+            Console.WriteLine(qqq);
+
+            var eee = Foo<Account<int, int>>();
+            Console.WriteLine(eee.ToString());
+        }
+
+        private void Swap<T>(ref T x, ref T y)
+        {
+            var temp = x;
+            x = y;
+            y = temp;
+        }
+
+        private T Foo<T>()
+        {
+            return default(T);
+        }
+
+        //private void Swap(ref int x, ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //private void Swap(ref string x, ref string y)
+        //{
+        //    string temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //private void Swap(ref Account<int, int> x, ref Account<int, int> y)
+        //{
+        //    Account<int, int> temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+    }
+}
--- BusStation/BusStation/Program.cs
+++ BusStation/BusStation/Program.cs
@@ -1,13 +1,65 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace BusStation
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello Bus Station!");
-            Console.WriteLine("Bye Bus Station!");
+            var gener = new GenerMeth();
+            gener.Main();
+            return;
+            var account1 = new Account<int, int> { Sum = 5000 };
+            var account2 = new Account<string, int> { Sum = 4000 };
+            account1.Id = 2;
+            account1.Age = 25;
+            Account<int, int>.session = 100;
+
+            account2.Id = "sadasd231";
+            //account2.Age = "Sorok";
+            account2.Age = 40;
+            Account<string, int>.session = "dvisti";
+
+            Console.WriteLine($"Acc 1 id = {account1.Id} and sum = {account1.Sum}. Age = {account1.Age}. Session = {Account<int, int>.session}");
+            Console.WriteLine($"Acc 2 id = {account2.Id} and sum = {account2.Sum}. Age = {account2.Age}. Session = {Account<string, int>.session}");
+
+            var myList = new List<int>();
+            myList.Add(12);
+
+            var arrayList = new ArrayList();
+            arrayList.Add(123);
+            arrayList.Add("qqq");
+            arrayList.Add(2.3f);
+
+            var accList = new List<Account<int, int>>();
+            accList.Add(account1);
+            //accList.Add(account2);
+
+            //var old1 = new AccountOld();
+            //AccountOld.session = 100;
+            //var old2 = new AccountOld();
+            //AccountOld.session = 200;
+            //var old3 = new AccountOld();
+
         }
     }
+
+    //class AccountOld
+    //{
+    //    public static int session;
+    //    public int Id { get; set; }
+    //    public int Sum { get; set; }
+    //}
+
+    class Account<Tvalue1, Tvalue2>
+    {
+        public static Tvalue1 session;
+
+        public Tvalue1 Id { get; set; }
+        public int Sum { get; set; }
+        public Tvalue2 Age { get; set; }
+    }
+
 }
