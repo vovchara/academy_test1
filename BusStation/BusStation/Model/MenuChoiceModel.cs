15eabfee21b533c08a53f3a532e1448357e686d8
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2021-11-08 21:17:16 +0200

    singleton and events

---
+++ BusStation/BusStation/Model/MenuChoiceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation.Model
+{
+    public class MenuChoiceModel
+    {
+        public int ChoiceId { get; }
+        public string ChoiceText { get; }
+
+        public MenuChoiceModel(int choiceId, string choiceText)
+        {
+            ChoiceId = choiceId;
+            ChoiceText = choiceText;
+        }
+    }
+}
---
+++ BusStation/BusStation/Model/MenuChoiceStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation.Model
+{
+    public class MenuChoiceStorage
+    {
+        public List<MenuChoiceModel> Choices { get; }
+
+        private static MenuChoiceStorage _instance;
+
+        private MenuChoiceStorage()
+        {
+            Choices = new List<MenuChoiceModel>
+            {
+                new MenuChoiceModel(1, "Show all"),
+                new MenuChoiceModel(2, "Find by Id"),
+                new MenuChoiceModel(3, "Find by destination")
+            };
+        }
+
+        public static MenuChoiceStorage GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new MenuChoiceStorage();
+            }
+            return _instance;
+        }
+
+    }
+}
